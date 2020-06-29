3f782e7ae8f44e78bab4019de394ddc79567c1a2
BenoitGaudieri <benalek@gmail.com>
2020-06-29 11:47:16 +0200

    closes #1 tags created

---
+++ database/seeds/TagTableSeeder.php
@@ -0,0 +1,29 @@
+<?php
+
+use App\Tag;
+use Illuminate\Database\Seeder;
+
+
+class TagTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $tags = [
+            "HTML",
+            "CSS",
+            "JS",
+            "Laravel"
+        ];
+
+        foreach ($tags as $tag) {
+            $newTag = new Tag();
+            $newTag->name = $tag;
+            $newTag->save();
+        }
+    }
+}
