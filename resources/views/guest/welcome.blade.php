675a05b8b6e253c7da1ce8d47ccf7cbe810bd87e
Francesco Scarniglia <francescoscarniglia@gmail.com>
2020-06-29 10:09:45 +0200

    First commit

---
+++ resources/views/guest/welcome.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+        <h1 class="mb-4"> Welcome guest!</h1>
+        </div>
+    </div>
+</div>
+@endsection
